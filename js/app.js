2d2f9b04c9c9208a3f4e16e50ac845c0ad0f5a98
Nan Santos <pixelado405@gmail.com>
2025-12-05 16:10:24 -0300

    Mudanças, melhorias e otimizações (Principalmente no JS)

--- js/app.js
+++ js/app.js
@@ -1,68 +1,49 @@
 let btnJanelaSobre = document.getElementById("sobre-mim-janela"),
     btnJanelaProjetos = document.getElementById("projetos-janela"),
+    btnJanelaConhecimentos = document.getElementById("icons-janela"),
+
     btnFecharSobre = document.getElementById("icon-fechar-sobre"),
     btnFecharProjetos = document.getElementById("icon-fechar-projetos"),
+    btnFecharConhecimentos = document.getElementById("icon-fechar-conhecimentos"),
+
     janelaSobreAberta = false,
-    janelaProjetosAberta = false
+    janelaProjetosAberta = false,
+    janelaConhecimentosAberta = false
 
 btnJanelaSobre.addEventListener("click", function(){
     if(janelaSobreAberta == false){
-        document.getElementById("col-sobre-mim").classList="col-12 order-1 mb-3"
-        document.getElementById("col-projetos").classList.remove("order-1")
-        document.getElementById("col-projetos").classList.remove("order-2")
-        document.getElementById("col-projetos").classList.add("order-2")
+        document.getElementById("texto-sobre-mim").classList.remove("texto-janela")
         janelaSobreAberta = true
     }
-    else if(janelaSobreAberta == true){
-        if(document.getElementById("col-sobre-mim").classList.contains("order-1")){
-            document.getElementById("col-sobre-mim").classList="col-lg-6 col-md-12 mb-3"
-            document.getElementById("col-projetos").classList.remove("order-1")
-            document.getElementById("col-projetos").classList.remove("order-2")
-            janelaSobreAberta = false
-        }
-        else{
-            document.getElementById("col-projetos").classList.remove("order-1")
-            document.getElementById("col-projetos").classList.add("order-2")
-            document.getElementById("col-sobre-mim").classList.remove("order-2")
-            document.getElementById("col-sobre-mim").classList.add("order-1")
-        }
-    }
-})
-
-btnJanelaProjetos.addEventListener("click", function(){
-    if(janelaProjetosAberta == false){
-        document.getElementById("col-projetos").classList="col-12 order-1 mb-3"
-        document.getElementById("col-sobre-mim").classList.remove("order-1")
-        document.getElementById("col-sobre-mim").classList.remove("order-2")
-        document.getElementById("col-sobre-mim").classList.add("order-2")
-        janelaProjetosAberta = true
-    }
-    else if(janelaProjetosAberta == true){
-        if(document.getElementById("col-projetos").classList.contains("order-1")){
-            document.getElementById("col-projetos").classList="col-lg-6 col-md-12 mb-3"
-            document.getElementById("col-sobre-mim").classList.remove("order-1")
-            document.getElementById("col-sobre-mim").classList.remove("order-2")
-            janelaProjetosAberta = false
-        }
-        else{
-            document.getElementById("col-sobre-mim").classList.remove("order-1")
-            document.getElementById("col-sobre-mim").classList.add("order-2")
-            document.getElementById("col-projetos").classList.remove("order-2")
-            document.getElementById("col-projetos").classList.add("order-1")
-        }
+    else{
+        document.getElementById("texto-sobre-mim").classList.add("texto-janela")
+        janelaSobreAberta = false
     }
 })
 
 btnFecharSobre.addEventListener("click", function(){
-    document.getElementById("col-sobre-mim").classList="col-lg-6 col-md-12 mb-3"
-    document.getElementById("col-projetos").classList="col-lg-6 col-md-12 mb-3"
+    document.getElementById("col-sobre-mim").classList.add("texto-janela")
     janelaSobreAberta = false
-    janelaProjetosAberta = false
 })
 
-btnFecharProjetos.addEventListener("click", function(){
-    document.getElementById("col-sobre-mim").classList="col-lg-6 col-md-12 mb-3"
-    document.getElementById("col-projetos").classList="col-lg-6 col-md-12 mb-3"
-    janelaSobreAberta = false
-    janelaProjetosAberta = false
+btnJanelaConhecimentos.addEventListener("click", function(){
+    if(janelaConhecimentosAberta == false){
+        document.getElementById("col-tecnologias").classList="col-12 mb-3"
+        janelaConhecimentosAberta = true
+    }
+    else{
+        document.getElementById("col-tecnologias").classList="col-lg-6 col-md-12 mb-3"
+        janelaConhecimentosAberta = false
+    }
+    if(document.getElementById("container-conhecimentos").classList.contains("icons-overflow") && janelaConhecimentosAberta == true){
+        document.getElementById("container-conhecimentos").classList.remove("icons-overflow")
+    }
+    else{
+        document.getElementById("container-conhecimentos").classList.add("icons-overflow")
+    }
+})
+
+btnFecharConhecimentos.addEventListener("click", function(){
+    document.getElementById("col-tecnologias").classList="col-lg-6 col-md-12 mb-3"
+    janelaConhecimentosAberta = false
 })
