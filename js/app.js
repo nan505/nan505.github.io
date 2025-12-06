b84f1fcd19dc215a6cc8244b6b1a6b9982cabbf5
Nan Santos <pixelado405@gmail.com>
2025-12-05 21:23:52 -0300

    Novas telas de carregamento!

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
+let loadingNumero = Math.floor(Math.random() * 3) + 1,
+    resultadoImg = document.getElementById("img-carregamento"),
+    resultadoTexto = document.getElementById("texto-carregamento")
+
+if(loadingNumero == 1){
+    resultadoImg.src="./imgs/Ice_Cream.webp"
+    resultadoImg.alt="Sea-salt ice cream"
+    resultadoTexto.innerHTML=`"You remember what this ice cream is called?"`
+}
+else if(loadingNumero == 2){
+    resultadoImg.src="./imgs/Axel_Chess_Piece.webp"
+    resultadoImg.alt="Axel's Chess Piece"
+    resultadoTexto.innerHTML=`"If you have a dream, don't wait. Act."`
+}
+else{
+    resultadoImg.src="./imgs/Roxas_Chess_Piece.webp"
+    resultadoImg.alt="Roxas's Chess Piece"
+    resultadoTexto.innerHTML=`"And who am I... supposed to be?"`
+}
+    
+
 window.addEventListener("load", function(){
     this.document.getElementById("carregamento-wrap").classList.add("carregamento-invisivel")
 
