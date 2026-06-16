20b6643fa428fed3927c692fe78ac75336fc55cb
Nan Santos <pixelado405@gmail.com>
2026-06-15 21:16:46 -0300

    Problema da scrollbar aparentemente corrigido!

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ window.addEventListener("load", function(){
     this.document.getElementById("carregamento-wrap").classList.add("carregamento-invisivel")
 
     this.document.getElementById("carregamento-wrap").addEventListener("transitionend", function(){
+        document.documentElement.classList.remove("body-sem-scroll")
         document.body.classList.remove("body-sem-scroll")
         document.getElementById("carregamento-wrap").classList.add("fundo-da-pagina")
         document.getElementById("img-carregamento").classList.remove("img-animacao")
